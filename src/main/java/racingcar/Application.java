e22f9bfdcbb167fda1915ca7689a847bda5c3b73
Jeongmin Park <60566546+jeongmin-0113@users.noreply.github.com>
2023-01-18 20:54:20 +0900

    feat(application): add functions for car names
    - add vector that include cars in main
    - add 2 function to input car names

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -1,7 +1,35 @@
 package racingcar;
+import java.util.Vector;
+import camp.nextstep.edu.missionutils.Console;
 
 public class Application {
-    public static void main(String[] args) {
-        // TODO 구현 진행
+    public void main(String[] args) {
+    	Vector<Car> cars = makeCarVector();
+
     }
+
+    public Vector<Car> makeCarVector() {
+    	String names[] = inputNames();
+
+    	Vector<Car> tempVector = new Vector();
+    	for (int i=0;i<names.length;i++) {
+    		Car temp = new Car(names[i]);
+    		tempVector.add(temp);
+    	}
+
+    	return tempVector;
+    }
+
+    public String[] inputNames() {
+    	String input_names;
+    	System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분) ");
+    	input_names = readLine();
+
+    	String name_array[] =input_names.split(",");
+    	for (int i=0;i<name_array.length;i++) {
+
+		}
+    	return name_array;
+    }
+
 }
